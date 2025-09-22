ae0937ec175adcfe5f9f946f9a90848ac2c1f7f5
Будаев Филипп <budaev.filipp@mail.ru>
2025-09-22 19:39:02 +0800

    Main, clean

--- main.py
+++ main.py
@@ -8,30 +8,26 @@ from extract_env import BASE_DIR
 
 
 def main():
-    if not BASE_DIR:
-        print('Завершение работы.')
-        sleep(5)
-        return None
-    main_dir = MainFolder(BASE_DIR)
-    dict_functions = {
-        'Create comics': main_dir.create_comic,
-        'Add chapter': main_dir.add_chapter,
-        'Extract image': main_dir.extract_image
-    }
-    while True:
-        action = pyip.inputMenu(
-            [key for key in dict_functions.keys()],
-            numbered=True,
-            prompt='Выберете команду:\n',
-            blank=True
-        )
-        if action:
-            answer = dict_functions[action]()
-            print(answer)
-        else:
-            print('Завершение работы.')
-            sleep(5)
+    if BASE_DIR:
+        main_dir = MainFolder(BASE_DIR)
+        dict_functions = {
+            'Create comics': main_dir.create_comic,
+            'Add chapter': main_dir.add_chapter,
+            'Extract image': main_dir.extract_image
+        }
+        while True:
+            action = pyip.inputMenu(
+                [key for key in dict_functions.keys()],
+                numbered=True,
+                prompt='Выберете команду:\n',
+                blank=True
+            )
+            if action:
+                answer = dict_functions[action]()
+                print(answer)
             break
+    print('Завершение работы.')
+    sleep(5)
 
 
 if __name__ == '__main__':
