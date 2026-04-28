2120768d0631fd98539d481acbb5d9c0622fb237
Будаев Филипп <budaev.filipp@mail.ru>
2026-04-28 16:09:32 +0800

    Window вынесена в отдельную функцию

--- graphical_interface.py
+++ graphical_interface.py
@@ -1,18 +1,25 @@
 import tkinter as tk
 from tkinter import filedialog
 
-# Окно
-window = tk.Tk()
-window.title('translate_comics')
-window.geometry("350x350")  # Ширина - высота
+name_env = (
+    'base_dir', 'original', 'editor', 'translate', 'additional_folders',
+    'length_number', 'delete'
+)
 
-# Столбцы и строки
-quantity_column = 2
-quantity_row = 8
-for index in range(quantity_column):
-    window.columnconfigure(index=index, weight=1)
-for index in range(quantity_row):
-    window.rowconfigure(index=index, weight=1)
+
+def create_window(column, row):
+    """column - столбцы, row - строки."""
+    window = tk.Tk()
+    window.title('translate_comics')
+    window.geometry("350x350")
+    for index in range(column):  # столбцы
+        window.columnconfigure(index=index, weight=1)
+    for index in range(row):  # строки
+        window.rowconfigure(index=index, weight=1)
+    return window
+
+
+window = create_window(2, 8)
 
 
 # Команды для Button
@@ -22,10 +29,13 @@ def setting():
     1) Создает из них файл .env
     2) Возвращает данные
     """
-    data = entries['BASE_DIR'].get()
-    test = filedialog.askdirectory()
-    print(test)
-    print(data)
+    for string in name_env[:-1]:
+        print(entries[string.upper()].get())
+    print(var.get())
+    # data = entries['BASE_DIR'].get()
+    # test = filedialog.askdirectory()
+    # print(test)
+    # print(data)
 
 
 # Валидация для Entry
@@ -40,10 +50,6 @@ entry_validate_wrapper = (window.register(validate_entry), '%P')
 
 # Виджеты
 labels = {}
-name_env = (
-    'base_dir', 'original', 'editor', 'translate', 'additional_folders',
-    'length_number', 'delete'
-)
 for string in name_env:
     upper = ' '.join(string.split('_')).upper()
     labels[string.upper()] = tk.Label(window, text=upper)
@@ -53,9 +59,9 @@ for string in name_env[:-2]:
     upper = ' '.join(string.split('_')).upper()
     entries[string.upper()] = tk.Entry(window)
 
-entries['length_number'] = tk.Spinbox(window, from_=1, to=5)
+entries[name_env[-2].upper()] = tk.Spinbox(window, from_=1, to=5)
 var = tk.BooleanVar()
-entries['delete'] = tk.Checkbutton(window, variable=var)
+entries[name_env[-1].upper()] = tk.Checkbutton(window, variable=var)
 
 buttons = {
     'SETTING': tk.Button(window, text='Сохранить', command=setting)
@@ -65,14 +71,15 @@ buttons = {
 entries['BASE_DIR'].insert(0, 'Симферополь')
 
 # Расположение виджетов
+grid_size = window.grid_size()
 for index, value in enumerate(labels):
     labels[value].grid(column=0, row=index, padx=20, sticky='w')
 for index, value in enumerate(entries):
     entries[value].grid(column=1, row=index, padx=20,)
 buttons['SETTING'].grid(
     column=0,
-    columnspan=quantity_column,
-    row=(quantity_row-1)
+    columnspan=grid_size[0],
+    row=grid_size[1] - 1
 )
 
 window.mainloop()
